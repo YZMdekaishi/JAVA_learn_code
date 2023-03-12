5bb284b0f9122d4cb03cea29505bfca7ecf31315
YangStorm <108247924+YZMdekaishi@users.noreply.github.com>
2023-03-12 15:32:30 +0800

    Update encapsulation.java

--- encapsulation.java
+++ encapsulation.java
@@ -8,7 +8,7 @@ class Account{
     String name = "";
     private double salary;
     private int password;
-
+//构造器
     public Account(String name, double salary, int password) {
         setName(name);
         setPassword(password);
@@ -18,7 +18,7 @@ class Account{
     public String getName() {
         return name;
     }
-
+//用于检验输入名字的字符是否大于2个字符，小于4个字符
     public void setName(String name) {
         if(name.length() >= 2 && name.length() <= 4){
             this.name = name;
@@ -27,11 +27,11 @@ class Account{
             System.out.println("你的输入有误, 默认名字：无名人");
         }
     }
-
+//输出salary
     public double getSalary() {
         return salary;
     }
-
+//用于检查salary是否大于20元
     public void setSalary(double salary) {
         if(salary >= 20){
             this.salary = salary;
@@ -44,7 +44,7 @@ class Account{
     public int getPassword() {
         return password;
     }
-
+//用于检查password是否是6位数字
     public void setPassword(int password) {
         if(password / 100_000 != 0){
             this.password = password;
